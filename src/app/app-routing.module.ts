04528e5fdabcb49923def2a5432e740a96fed801
donghia1994 <55423896+donghia1994@users.noreply.github.com>
2019-11-14 22:33:58 +0700

    lab2
    lab2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { AdminComponent} from './admin/admin.component';
+const routes: Routes = [
+    { path: '', component: HomeComponent  },
+    { path :'product', component: ProductComponent },
+    { path:'admin', component: AdminComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
